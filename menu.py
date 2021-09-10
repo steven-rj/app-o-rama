31a83b1c77e4903fdc7332b3b9be8d01da493be6
Steven Jordan <stevenrjfiles@gmail.com>
2021-09-10 08:05:54 -0400

    created functions to get username and clear screen

--- menu.py
+++ menu.py
@@ -1,10 +1,23 @@
+import os
 
-
-def display_menu():
+def display_menu(username):
     
+    username = username
     choice = ""
+    choices = ["q"]
+
+    while choice not in choices:
+        clear_screen()
+        print(f"Hello {username}! Below are the following apps.")
+        print("Q to quit")
+        choice = input("Press the key to the corresponding app you want, then pess Enter: ").casefold()
 
     run_app(choice)
+
+
+def get_username():
+    username = input("Enter your name: ")
+    return username
 
 
 def run_app(choice):
@@ -13,5 +26,15 @@ def run_app(choice):
         exit()
 
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def main():
+
+    username = get_username()
+    display_menu(username)
+
+
 if __name__ == "__main__":
-    display_menu()
+    main()
