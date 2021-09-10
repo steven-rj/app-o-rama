c4c27c6995c30da89bdf693d9b6b2472940821a9
Steven RJ <stevenrjfiles@gmail.com>
2021-09-09 22:14:58 -0400

    initial commit

---
+++ menu.py
@@ -0,0 +1,17 @@
+
+
+def display_menu():
+    
+    choice = ""
+
+    run_app(choice)
+
+
+def run_app(choice):
+    
+    if choice == 'q':
+        exit()
+
+
+if __name__ == "__main__":
+    display_menu()
