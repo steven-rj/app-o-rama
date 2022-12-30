f6678249fdfe1c57fcb2532f8eefe38a1c168207
steven-rj <stevenrjfiles@gmail.com>
2022-12-30 18:52:38 -0500

    refactored main_menu's helper functions

--- main_menu.py
+++ main_menu.py
@@ -1,8 +1,8 @@
 import sys
 import time
 import os
-import organize_apps as apps
 
+import organize_apps as apps
 import youtube_downloader as yt
 import pdf_speech as pdf
 import bigclipboard as clipb
@@ -10,6 +10,10 @@ import bigclipboard as clipb
 
 def clear_screen():
     os.system('cls')
+
+
+def wait_timer():
+    time.sleep(1)
 
 
 def display_main_menu():
@@ -20,8 +24,7 @@ def display_main_menu():
     print()
     print("=" * 15)
     print("App-o-Rama v0.2")
-    print("=" * 15)
-    print()
+    print("=" * 15, "\n")
 
     for option in options:
         app_num = option[0]
@@ -30,10 +33,14 @@ def display_main_menu():
 
     print()
 
-    choice = int(input("Enter number for selection: "))
-    
-    program = options[choice][1][1]
-    exec(program)
+    choice = int(input("Enter number for selection: "))    
+    program_file = options[choice][1][1]
+    program_name = options[choice][1][0]
+
+    print(f"\nLoading {program_name}..")
+    wait_timer()
+    clear_screen()
+    exec(program_file)
 
 
 if __name__ == "__main__":
