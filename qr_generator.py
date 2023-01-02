3191f973287e6c22140f7a00fd8248fe8998f824
steven-rj <stevenrjfiles@gmail.com>
2023-01-02 15:18:54 -0500

    added custom save path to QR Generator script

--- qr_generator.py
+++ qr_generator.py
@@ -3,7 +3,7 @@ import main_menu
 import qrcode
 
 
-def generate_code(text):
+def generate_code(text, path):
 
     qr = qrcode.QRCode(version = 1,
                         error_correction = qrcode.constants.ERROR_CORRECT_L,
@@ -13,12 +13,21 @@ def generate_code(text):
     qr.add_data(text)
     qr.make(fit=True)
     image = qr.make_image(fill_color = "black", back_color = "white")
-    image.save("qrcode.png")
+    image.save(path + "\qrcode.png")
+
+
+def get_download_path():
+
+    file_path = input("Enter path to save the QR code: ")
+    return file_path
+
 
 def main():
     
     text = input("Enter text to have generated into a QR code: ")
-    generate_code(text)
+    path = get_download_path()
+    generate_code(text, path)
+    print(f"QR code created successfully at {path}")
     main_menu.display_main_menu()
 
 if __name__ == "__main__":
